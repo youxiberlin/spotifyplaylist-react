57a6cc4dca9174dfd59cbfdce2055fd9793e8a84
Yuki Sato <sato.youxi@gmail.com>
2018-10-09 16:42:03 +0200

    start with create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let defaultStyle = {
+  color: '#fff'
+};
+
+class Aggregate extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div>
+        <img />
+        <input type="text" />
+      </div>
+    )
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div style={{ ...defaultStyle, display: 'inline-block', width: "25%" }}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song1</li>
+          <li>Song2</li>
+          <li>Song3</li>
+        </ul>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Title</h1>
+        <Aggregate />
+        <Filter />
+        <Playlist />
+        <Playlist />
+        <Playlist />
       </div>
     );
   }
